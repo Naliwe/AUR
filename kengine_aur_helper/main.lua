409fbd63850de08349faa0cef8caf260b8e7f95d
Naliwe <r.fourdrilis@gmail.com>
2017-07-11 21:41:07 +0200

    Started kengine helper

---
+++ kengine_aur_helper/main.lua
@@ -0,0 +1,13 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: naliwe
+-- Date: 7/11/17
+-- Time: 9:23 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require "classes/repository"
+
+r = Repo:new()
+
+print(r.remote)
