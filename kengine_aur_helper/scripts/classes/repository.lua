c05b8f30b8e0cbadbaf936b1d6082e383bbf25ff
Naliwe <r.fourdrilis@gmail.com>
2017-07-12 04:12:15 +0200

    added aur specific files

--- kengine_aur_helper/scripts/classes/repository.lua
+++ kengine_aur_helper/scripts/classes/repository.lua
@@ -9,9 +9,17 @@
 local lfs = require "lfs"
 
 Repo = {
-    name = "default",
+    remoteDir = "",
     remote = "default",
-    localPath = "default"
+    localPath = "default",
+    tmpDir = "/tmp/kengine_helper",
+    submodule = false,
+    cached = true,
+    cachedDir = tmpDir,
+    branch = "master",
+    postCreate = function()
+        print('Initial setup finished.')
+    end
 }
 
 function Repo:new(other)
@@ -23,13 +31,74 @@ function Repo:new(other)
     return other
 end
 
+local function _createTmpDir(repo)
+    if (lfs.attributes(repo.tmpDir) == nil) then
+        lfs.mkdir(repo.tmpDir)
+    end
+end
+
+local function Set(list)
+    local set = {}
+    for _, l in ipairs(list) do set[l] = true end
+    return set
+end
+
+local matchers =
+Set { ".git", ".gitignore", ".gitmodules", ".idea" }
+
+local function _delGitFiles(path)
+    for file in lfs.dir(path) do
+        if (matchers[file]) then
+            os.execute("rm -rf " .. path .. '/' .. file)
+        else
+            if file ~= "." and file ~= ".." then
+                local f = path .. '/' .. file
+                local attr = lfs.attributes(f)
+
+                if attr.mode == "directory" then
+                    _delGitFiles(f)
+                end
+            end
+        end
+    end
+end
+
 local function _populateRepo(repo)
     print(repo.localPath)
+
+    _createTmpDir(repo)
+
+    if (lfs.attributes(repo.cacheDir) ~= nil) then
+        os.execute("cp -r " .. repo.cacheDir .. "/" .. repo.remoteDir .. " " .. repo.localPath)
+        return
+    end
+
+    os.execute("git clone " .. repo.remote .. " " .. repo.tmpDir .. "/" .. repo.remoteDir .. " --recursive")
+
+    local tmp = lfs.currentdir()
+    lfs.chdir(repo.tmpDir .. "/" .. repo.remoteDir)
+
+    os.execute("git checkout " .. repo.branch)
+
+    if (not repo.submodule) then
+        _delGitFiles(lfs.currentdir())
+    end
+
+    lfs.chdir(tmp)
+
+    if (repo.cached and repo.cacheDir ~= repo.tmpDir) then
+        if (lfs.attributes(repo.cacheDir) == nil) then
+            lfs.mkdir(repo.cacheDir)
+        end
+        os.execute("cp -r " .. repo.tmpDir .. "/" .. repo.remoteDir .. " " .. repo.cacheDir)
+    end
+
+    os.execute("cp -r " .. repo.tmpDir .. "/" .. repo.remoteDir .. " " .. repo.localPath)
 end
 
 function Repo:create()
     if (lfs.attributes(self.localPath) == nil) then
-        print('creating folder '..self.localPath)
+        print('creating folder ' .. self.localPath)
         print('Confirm ? Y/n')
         local confirm = io.read("*line")
 
@@ -39,4 +108,8 @@ function Repo:create()
 
         lfs.mkdir(self.localPath)
     end
+
+    _populateRepo(self)
+
+    self.postCreate()
 end
--- kengine_aur_helper/scripts/main.lua
+++ kengine_aur_helper/scripts/main.lua
@@ -1,3 +1,5 @@
+#!/usr/bin/env lua
+
 --
 -- Created by IntelliJ IDEA.
 -- User: naliwe
@@ -7,8 +9,21 @@
 --
 
 local lfs = require "lfs"
+local argparse = require "argparse"
+local parser = argparse()
+local savePath = lfs.currentdir()
 
-local defaultPath = lfs.currentdir()
+parser:argument("localPath", "The location you want the repo to be created at"):default(savePath)
+parser:argument("remoteDir", "The engine folder name"):default "engine"
+
+parser:option "--tmp":default "/tmp/kengine_helper"
+parser:option "-n --name":default "example"
+parser:flag("-s --submodule"):description "Should I install the engine as a git submodule ?":default(false)
+
+parser:mutex(parser:option("-c --cache", "Where you want me to cache deps"):default(os.getenv("HOME") .. "/.config/kengine_helper"),
+    parser:flag("--no-cache", "Do not cache deps"):default(false))
+
+local args = parser:parse()
 local index = string.find(arg[0], "/[^/]*$")
 
 lfs.chdir(string.sub(arg[0], 0, index))
@@ -17,15 +32,40 @@ require "classes.repository"
 
 local path = ""
 
-if (arg[1] ~= nil) then
-    if (string.find(arg[1], "/") == nil) then
-        path = defaultPath .. "/" .. arg[1]
+if (args.localPath ~= nil) then
+    if (string.find(args.localPath, "/") == nil) then
+        path = savePath .. "/" .. args.localPath
     else
-        path = arg[1]
+        path = args.localPath
     end
-else
-    path = defaultPath;
 end
 
-r = Repo:new({ localPath = path })
+r = Repo:new({
+    remote = "git@github.com:Phiste/kengine.git",
+    remoteDir = args.remoteDir,
+    localPath = path,
+    tmpDir = args.tmp,
+    cached = not args.nocache,
+    cacheDir = args.cache,
+    submodule = args.submodule,
+    branch = "kengine_aur",
+    postCreate = function()
+        os.execute("cp -r " .. r.localPath .. '/' .. r.remoteDir .. "/aur_utils/* " .. r.localPath)
+        local pos = 0
+        local cmakelistsPath = r.localPath .. '/' .. "CMakeLists.txt"
+        local cmakeLines = "set(project_name " .. args.name .. ")\nset(engine_dir " .. r.remoteDir .. ")"
+
+        for line in io.lines(cmakelistsPath) do
+            pos = pos + string.len(line)
+            if (line == "# helper_placeholder") then
+                break
+            end
+        end
+
+        local handle = io.open(cmakelistsPath)
+        handle:seek("set", pos)
+        handle:write(cmakeLines)
+        io.close(handle)
+    end
+})
 r:create()
